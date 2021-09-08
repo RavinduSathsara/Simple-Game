400135a88f898be507a9a1e63b3ba824da1de89a
RavinduSathsara <rsathsara330@gmail.com>
2021-09-08 13:00:57 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+      Player1 p = new Player1("Ravindu Sathsara", "M416", 100);
+
+      p.damageByGun1();
+      p.damageByGun1();
+      p.heal();
+      p.damageByGun1();
+      p.damageByGun2();
+      p.damageByGun2();
+      p.heal();
+
+
+
+    }
+}
